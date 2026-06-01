6a3c99c6e51686fdbd13ba51dada05a8b50e28b1
dipriv <diprivxxx@gmail.com>
2026-06-01 14:35:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const USERS_DATABASE = {
     "dipriv": { 
         password: "arcnet215", 
         defaultColor: "#e74c3c",
-        firebaseUrl: "https://dipriv-47697-default-rtdb.firebaseio.com/midias.json",
+        firebaseUrl: "https://dipriv-47697-default-rtdb.firebaseio.com/.json",
         ytApiKey: "AIzaSyD2x7SjdblFqlxQdKHlgfSZA5Nmjb1QbMk"
     }
 };
